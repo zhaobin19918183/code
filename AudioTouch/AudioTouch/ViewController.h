2f0a53373a508e61e6c6fe6ff68bf0dec6e70507
newland <newland@newlanddeMac.local>
2017-05-31 18:00:23 +0800

    no message

--- AudioTouch/AudioTouch/ViewController.h
+++ AudioTouch/AudioTouch/ViewController.h
@@ -9,17 +9,46 @@
 #import <UIKit/UIKit.h>
 #import "LineChartCell.h"
 
+#include "EZAudio/EZAudio.h"
+
 @protocol DBDelegate <NSObject>
 
 -(void)getDB:(float)db;
 
 @end
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<EZMicrophoneDelegate, EZAudioFFTDelegate>
+
 typedef void(^SelectedRoomBlock)(NSString *hzStr);
 @property (nonatomic, copy) SelectedRoomBlock selectedRoomBlock;
 /**代理*/
 @property(nonatomic,weak)id<DBDelegate>delegate;
+
+// EZAudioPlot for frequency plot
+//
+@property (nonatomic,strong)  EZAudioPlot *audioPlotFreq;
+
+//
+// EZAudioPlot for time plot
+//
+@property (nonatomic,strong)  EZAudioPlot *audioPlotTime;
+
+////
+//// A label used to display the maximum frequency (i.e. the frequency with the
+//// highest energy) calculated from the FFT.
+////
+//@property (nonatomic, strong)  UILabel *maxFrequencyLabel;
+
+//
+// The microphone used to get input.
+//
+@property (nonatomic,strong) EZMicrophone *microphone;
+
+//
+// Used to calculate a rolling FFT of the incoming audio data.
+//
+@property (nonatomic, strong) EZAudioFFTRolling *fft;
+
 
 @end
 
--- AudioTouch/AudioTouch/ViewController.mm
+++ AudioTouch/AudioTouch/ViewController.mm
@@ -149,8 +149,18 @@ void AudioCallback( Float32 * buffer, UInt32 frameSize, void * userData )
 
 
 @end
-
+static vDSP_Length const FFTViewControllerFFTWindowSize = 4096;
 @implementation ViewController
+
+
+//------------------------------------------------------------------------------
+#pragma mark - Status Bar Style
+//------------------------------------------------------------------------------
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -163,7 +173,61 @@ void AudioCallback( Float32 * buffer, UInt32 frameSize, void * userData )
     initializeAccumulator();
     [self initMomuAudio];
     [self dbAction];
+    [self hzAudio];
 }
+
+-(void)hzAudio
+{
+    //
+    // Setup the AVAudioSession. EZMicrophone will not work properly on iOS
+    // if you don't do this!
+    //
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    NSError *error;
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
+    if (error)
+    {
+        NSLog(@"Error setting up audio session category: %@", error.localizedDescription);
+    }
+    [session setActive:YES error:&error];
+    if (error)
+    {
+        NSLog(@"Error setting up audio session active: %@", error.localizedDescription);
+    }
+    
+    //
+    // Setup time domain audio plot
+    //
+    self.audioPlotTime.plotType = EZPlotTypeBuffer;
+    self.audioPlotTime.gain =30.0;
+
+    
+    //
+    // Setup frequency domain audio plot
+    //
+    self.audioPlotFreq.shouldFill = YES;
+    self.audioPlotFreq.plotType = EZPlotTypeBuffer;
+    self.audioPlotFreq.shouldCenterYAxis = NO;
+//
+//    //
+//    // Create an instance of the microphone and tell it to use this view controller instance as the delegate
+//    //
+   self.microphone = [EZMicrophone microphoneWithDelegate:self];
+//    
+//    //
+//    // Create an instance of the EZAudioFFTRolling to keep a history of the incoming audio data and calculate the FFT.
+//    //
+   self.fft = [EZAudioFFTRolling fftWithWindowSize:FFTViewControllerFFTWindowSize
+                                         sampleRate:self.microphone.audioStreamBasicDescription.mSampleRate
+                                          delegate:self];
+//
+//    //
+//    // Start the mic
+//    //
+   [self.microphone startFetchingAudio];
+
+}
+
 -(void)dbAction
 {
     [[AVAudioSession sharedInstance]
@@ -283,6 +347,46 @@ void AudioCallback( Float32 * buffer, UInt32 frameSize, void * userData )
     cell.backgroundColor = [UIColor clearColor];
     cell.delegate = self;
     return cell;
+}
+//------------------------------------------------------------------------------
+#pragma mark - EZMicrophoneDelegate
+//------------------------------------------------------------------------------
+
+-(void)    microphone:(EZMicrophone *)microphone
+     hasAudioReceived:(float **)buffer
+       withBufferSize:(UInt32)bufferSize
+ withNumberOfChannels:(UInt32)numberOfChannels
+{
+    //
+    // Calculate the FFT, will trigger EZAudioFFTDelegate
+    //
+    [self.fft computeFFTWithBuffer:buffer[0] withBufferSize:bufferSize];
+    
+    __weak typeof (self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [weakSelf.audioPlotTime updateBuffer:buffer[0]
+                              withBufferSize:bufferSize];
+    });
+}
+
+//------------------------------------------------------------------------------
+#pragma mark - EZAudioFFTDelegate
+//------------------------------------------------------------------------------
+
+- (void)        fft:(EZAudioFFT *)fft
+ updatedWithFFTData:(float *)fftData
+         bufferSize:(vDSP_Length)bufferSize
+{
+    float maxFrequency = [fft maxFrequency];
+    NSString *noteName = [EZAudioUtilities noteNameStringForFrequency:maxFrequency
+                                                        includeOctave:YES];
+    
+    __weak typeof (self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+       NSString *string = [NSString stringWithFormat:@"Highest Note: %@,\nFrequency: %.2f", noteName, maxFrequency];
+        NSLog(@"%@",string);
+        [weakSelf.audioPlotFreq updateBuffer:fftData withBufferSize:(UInt32)bufferSize];
+    });
 }
 
 -(void)reloadView{
